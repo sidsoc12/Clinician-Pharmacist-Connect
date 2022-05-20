8739b4f36d0821cfe2e6ccd84ed0592ed57a17d9
Siddharth Potti <64938719+sidsoc12@users.noreply.github.com>
2022-05-19 21:02:49 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+// import { initializeApp } from "firebase/app"
+// import { getAuth } from "firebase/auth"
+// import { getFirestore } from "firebase/firestore"
+// import { getStorage } from "firebase/storage"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+// const store = app.storage();
+const auth = firebase.auth();
+
+export { firebase, db, auth};
